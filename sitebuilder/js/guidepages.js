b3558d805fce61a372cffd07548b59c09032e392
philip tutty <p.tutty@warwick.ac.uk>
2018-11-28 11:23:30 +0000

    added more sitebuilder templates and widgets

--- sitebuilder/js/guidepages.js
+++ sitebuilder/js/guidepages.js
@@ -10,9 +10,25 @@ How to write a CV (parent)
 ---- Target your CV (child)
 ---- format your CV (child)
 
-Each child page has sitebuilder pagination turned on to navifate between subpages
+Each child page has sitebuilder pagination turned on to navigate between subpages
 
 */
+
+jQuery( document ).ready(function($) {
+
+    guideBuilder.buildNav();
+    guideBuilder.buildPagination();
+
+
+	// this is a hack for h1 formatting hopegully take out if web team can build template to accommodate this
+    var h1_text = $("title").text().split(':')[0];
+    jQuery("h2.guide-topic-title").replaceWith("<h1 class='guide-topic-title'>" + h1_text +  "</h1>");
+});
+
+
+
+
+// main functions in JS module pattern
 
 var guideBuilder = (function() {
   var fullURL = window.location.href;
@@ -27,10 +43,6 @@ var guideBuilder = (function() {
   const parentsnippet = "table.tableOfContentsTable"; // this is the table sitebuilder generates
   const childtarget = "#guide_content_list"; // where to load snippet from parent
 
-
-
-
-  /* methods */
 
   // get the nav
   var getParentNav = function() {
@@ -51,7 +63,7 @@ var guideBuilder = (function() {
 
 
   var pagination = function() {
-        jQuery("ul.pager").appendTo("nav.scs-guide-pagination");
+        jQuery("ul.pager").appendTo(".scs-content-main-block");
         var $next = jQuery("ul.pager > li.next");
         var $previous = jQuery("ul.pager > li.previous");
         extractTitle($next ,"next");
@@ -67,6 +79,8 @@ var guideBuilder = (function() {
             var title;
             if (direction == "next" ) {
               title = jQuery("ul.pager > li.next > a").attr('title');
+            
+
               jQuery("ul.pager > li.next > a").html("<strong>Next</strong></br>" + split(title));
             } else {
               title = jQuery("ul.pager > li.previous > a").attr('title');
@@ -81,20 +95,9 @@ var guideBuilder = (function() {
           }
   }
 
-
   return {
     buildNav: getParentNav,
     buildPagination: pagination
   }
 
 })();
-
-
-jQuery( document ).ready(function($) {
-
-    guideBuilder.buildNav();
-    guideBuilder.buildPagination();
-
-    var h1_text = $("title").text().split(':')[0];
-    jQuery("h2.guide-topic-title").replaceWith("<h1 class='guide-topic-title'>" + h1_text +  "</h1>");
-});
