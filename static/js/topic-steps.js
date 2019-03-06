d5d741437a922067466891b6eb3eb845454534a4
philip tutty <p.tutty@warwick.ac.uk>
2019-03-06 09:04:20 +0000

    site builder stuff

--- static/js/topic-steps.js
+++ static/js/topic-steps.js
@@ -1,90 +1,81 @@
-/* Topic section with multi part steps that show/hide */
-
-var d = {};
-d.hideText = "Hide";
-d.showText = "Show";
-var showAllPanels = false;
-var panelArray  = [];
 
 
-jQuery( document ).ready(function($) {
+/* for Topic sections with multi part steps that show/hide and save state */
+var careersAppTopicSteps =  (function ($, window) {
 
-  // toogle panels previously open before page refresh
-  getStoredPanelArray();
+  var showAllPanels = false;
+  var panelArray  = [];
 
-  /* toogle all step details */
-  $(".js-step-controls-button").on("click", function() {
+  function init() {
+    // wait until DOM is loaded
+    $(function() {
+      // load persistent data from local storage
+      getStoredPanelArray();
 
-    // show hide all panels
-    var $allpanels = $(".scs-c-step-nav__steps").find(".js-panel")
-    $allpanels.toggleClass("js-hidden", showAllPanels);
-    showAllPanels = !showAllPanels;
+      // show/hide all button event handler
+      $(".js-step-controls-button").on("click", function() {
+        var $allpanels = $(".scs-c-step-nav__steps").find(".js-panel")
+        $allpanels.toggleClass("js-hidden", showAllPanels);
+        showAllPanels = !showAllPanels;
+        $(this).html(showAllPanels ? "Hide All" : "Show All");
+        $(".js-toggle-link").html(showAllPanels ?  "Hide" : "Show");
+        // persist state
+        panelArray  = [];
+        if (showAllPanels) {
+          $allpanels.each(function() {
+            panelArray.push($(this).attr('id'));
+          });
+        }
+        localStorage.setItem("panels", JSON.stringify(panelArray));
+      });
 
-    // change button text
-    $(this).html(showAllPanels ? "Hide All" : "Show All").end();
-    $(".js-toggle-link").html(showAllPanels ? d.hideText : d.showText);
+      // individual steps event handler
+      $(".js-toggle-panel").click(function() {
+          var $parent = $(this).parent("li.js-step");
+          var $panel = $parent.find(".js-panel");
+          var $tooglelink = $parent.find(".js-toggle-link");
 
-    // add/remove all ID's to the array for save state
-    $allpanels.each(function( index ) {
-        addPanelToArray( $(this).attr('id') );
+          if ($panel.hasClass("js-hidden")) {
+              $panel.toggleClass("js-hidden");
+              $tooglelink.html("hide");
+          } else {
+             $panel.toggleClass("js-hidden");
+             $tooglelink.html("show");
+          }
+          // persist state in local storage
+          addPanelToArray($panel.attr('id'));
+      });
     });
+  };
+  init();
 
-  });
+  // get saved panel state in local storage
+  function getStoredPanelArray() {
+    var previouslyopenpanels =  JSON.parse(localStorage.getItem("panels"));
+    if ( previouslyopenpanels  === null ) {
+      panelArray  = [];
+    } else {
+      panelArray  = previouslyopenpanels;
+      panelArray.forEach(function(element) {
+        $panel = $(".scs-c-step-nav__steps").find("#" + element);
+        $panel.toggleClass("js-hidden");
+        $panel.parent(".scs-c-step-nav__step").find(".js-toggle-link").html("hide");
+        showAllPanels = !showAllPanels;
+        $(".js-step-controls-button").html(showAllPanels ? "Hide All" : "Show All").end();
+      })
+    }
+  }
 
-
-  /* toggle each step detail */
-  $(".js-toggle-panel").click(function() {
-      var e = $(this).parent("li.js-step");
-      var $details = e.find(".js-panel");
-
-      // add clicked panel ID to localStorage
-      addPanelToArray( $details.attr('id') );
-
-      if ( $details.hasClass("js-hidden") ) {
-          $details.toggleClass("js-hidden");
-          e.find(".js-toggle-link").html("hide");
-      } else {
-         $details.toggleClass("js-hidden");
-         e.find(".js-toggle-link").html("show");
-      }
-
-  });
-
-});
-
-
-/* save state of panels in array and then local storage */
-function addPanelToArray(panel) {
-  let index = panelArray.indexOf(panel);
-    if (index >= 0) { // panel is already present remove it ..
+  // save state of panels in array and then local storage
+  function addPanelToArray(panel) {
+    var index = panelArray.indexOf(panel);
+    if (index >= 0) {
         panelArray.splice(index, 1);
-    } else { // add to array
+    } else {
         panelArray.push(panel);
     }
-    console.log( panelArray );
-    // update local storage
     localStorage.setItem("panels", JSON.stringify(panelArray));
-}
-
-/* get saved panel state in local storage */
-function getStoredPanelArray() {
-  let previouslyopenpanels =  JSON.parse(localStorage.getItem("panels"));
-  if ( previouslyopenpanels  === null ) {
-    panelArray  = [];
-  } else {
-    panelArray  = previouslyopenpanels;
-
-    // iterate over array and show panels previously left open
-    panelArray.forEach(function(element) {
-      $panel = jQuery(".scs-c-step-nav__steps").find("#" + element);
-      $panel.toggleClass("js-hidden");
-      $panel.parent(".scs-c-step-nav__step").find(".js-toggle-link").html("hide");
-
-      // change button text
-      showAllPanels = !showAllPanels;
-      jQuery(".js-step-controls-button").html(showAllPanels ? "Hide All" : "Show All").end();
-
-
-    });
   }
-}
+
+
+} (jQuery , window) );
